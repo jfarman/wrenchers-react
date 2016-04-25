fe89090cb3a48b1b00469c36994e5beca08dc095
jfarman <jfarman@stanford.edu>
2016-04-24 21:43:03 -0700

    Completed: React demo with working Firebase connection (5 hours)

--- scripts/main.js
+++ scripts/main.js
@@ -15,8 +15,9 @@ var base = Rebase.createClass('https://blinding-heat-6110.firebaseio.com/');
 
 /*
   App
+  Usage: component={app}
+  This is the main application, which is accessed via a valid tutorial ID.
 */
-
 var App = React.createClass({
   getInitialState : function() {
     return {
@@ -26,6 +27,7 @@ var App = React.createClass({
     }
   },
   componentDidMount : function() {
+    // There's probably a better way of doing this
     base.syncState(this.props.params.tutorialId + '/steps', {
       context : this,
       state : 'steps'
@@ -44,12 +46,17 @@ var App = React.createClass({
       <div className="wrenchers-tutorial">
         <WrenchersHeader customer={this.state.customer} tutorial={this.state.tutorialName} />
         <TutorialSteps steps={this.state.steps} />
-        {/*<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>*/}
+        <Footer />
       </div>
     )
   }
 });
 
+/*
+  TutorialSteps
+  Usage: <TutorialSteps/>
+  Renders all steps for a given tutorial.
+*/
 var TutorialSteps = React.createClass({
   renderStep : function(key){
     return <Step key={key} index={key} details={this.props.steps[key]}/>
@@ -63,6 +70,11 @@ var TutorialSteps = React.createClass({
   }
 })
 
+/*
+  Step
+  Usage: <Step/>
+  Renders an individual step within the tutorial.
+*/
 var Step = React.createClass({
   render : function() {
     var details = this.props.details;
@@ -71,11 +83,9 @@ var Step = React.createClass({
     return (
       <div>
         <div className="page-header" id="step1">
-          {/*<img src={details.image} alt={details.name} />*/}
           <h3>{details.stepName}<br/>
             <small>{details.subtitle}</small>
           </h3>
-          {/*<p>{details.desc}</p>*/}
         </div>
         {alert && <div className="alert alert-info" role="alert">{alert}</div> }
         {components && <Components components={details.components} />}
@@ -84,6 +94,203 @@ var Step = React.createClass({
   }
 })
 
+/*
+  Components
+  Usage: <Components/>
+  Renders all the components for a given step in the tutorial.
+*/
+var Components = React.createClass({
+  // Render method for a regular, non-featured component.
+  renderComponent : function(key){
+    return <OtherComponent key={key} index={key} details={this.props.components.otherComponents[key]}/>
+  },
+  render : function() {
+    var featured = this.props.components.featuredComponent;
+    var other = this.props.components.otherComponents;
+    var tools = this.props.components.toolsList;
+    // These classes are abstracted out here so that they can be easily modified.
+    var fClass = "col-sm-6 col-md-3";
+    var oClass = "col-sm-6 col-md-9";
+
+    return (
+      <div className="row">
+          {featured &&  // if this component contains a featured component, render it
+            <div className={fClass}>
+              <FeaturedComponent component={featured}/>
+            </div>
+          }
+          {other && // if this component contains any other non-featured components, render it
+            <div className={oClass}>
+              {Object.keys(other).map(this.renderComponent)}
+            </div>
+          }
+          {tools && // if this component contains a tools list component, render it
+            <ToolsList tools={tools}/>
+          }
+      </div>
+    )
+  }
+})
+
+/*
+  FeaturedComponent
+  <FeaturedComponent/>
+  Renders a super awesome featured component.
+*/
+var FeaturedComponent = React.createClass({
+  render : function() {
+    return (
+        <div className="thumbnail">
+          {(() => {
+            switch (this.props.component.componentType) {
+              case "image": return (<FeaturedImage component={this.props.component}/>);
+              case "video": return (<FeaturedVideo component={this.props.component}/>);
+              // Featured items should only be either images or videos, but this will return
+              // an empty div in case that FeaturedComponent is invoked improperly.
+              default:      return (<div></div>);
+            }
+          })()}
+        </div>
+    )
+  }
+})
+
+/*
+  OtherComponent
+  Usage: <OtherComponent/>
+  Renders a regular non-featured component.
+*/
+var OtherComponent = React.createClass({
+  render : function() {
+    var styles = {height: '120px',width: '120px'};
+    var details = this.props.details;
+    var image = details.image;
+    return (
+      <div className="media">
+        <div className="media pull-left">
+          {image && // if this component has a corresponding image, render it
+            <a href={details.contentLink}>
+              <img className="media-object" src={image} alt="..." style={styles}/>
+            </a>
+          }
+        </div>
+        <div className="media-body">
+          <h4 className="media-heading">{details.heading}</h4>
+          <p>{details.textPreview}<a href={details.contentLink}>Read More</a></p>
+        </div>
+      </div>
+    )
+  }
+})
+
+/*
+  Caption
+  Usage: <Caption/>
+  Renders captions for the images and videos within featured components.
+*/
+var Caption = React.createClass({
+  render : function() {
+    return (
+      <div className="caption">
+        <h4>{this.props.text}</h4>
+        <p>From <a href={this.props.link}>{this.props.source}</a></p>
+      </div>
+    )
+  }
+})
+
+/*
+  FeaturedVideo
+  Usage: <FeaturedVideo/>
+  Renders a video within a featured component.
+*/
+var FeaturedVideo = React.createClass({
+  render : function() {
+    var details = this.props.component;
+    return (
+      <div>
+        <div className="embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" src={details.videoSource}></iframe>
+        </div>
+        <Caption text={details.captionText} link={details.contentLink} source={details.contentLinkSource} />
+      </div>
+    )
+  }
+})
+
+/*
+  FeaturedImage
+  Usage: <FeaturedImage/>
+  Renders an image within a featured component.
+*/
+var FeaturedImage = React.createClass({
+  render : function() {
+    var details = this.props.component;
+    return (
+      <div>
+        <a href={details.imagePath}>
+            <img src={details.imagePath} alt="..."/>
+        </a>
+        <Caption text={details.captionText} link={details.contentLink} source={details.contentLinkSource} />
+      </div>
+    )
+  }
+})
+
+/*
+  Tool
+  Usage: <Tool/>
+  Renders information for a specific tool.
+*/
+var Tool = React.createClass({
+  render : function() {
+    var details = this.props.details;
+    var image = details.image;
+    var imageStyles = {height: '225px'};
+    return (
+      <div className="col-sm-6 col-md-3">
+        <div className="thumbnail">
+            {image && // if this component has a corresponding image, render it
+              <a href={details.itemLink}>
+                <img src={image} alt="..." style={imageStyles}/>
+              </a>
+            }
+            <div className="caption download-block text-center">
+              <h5>{details.itemName}</h5>
+              <p>
+                <a href={details.itemLink} className="btn-download" role="button">{details.purchasingInfo}</a> 
+              </p>
+            </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+/*
+  ToolsList
+  Usage: <ToolsList/>
+  Renders information for a tool, including image and a link to purchase.
+*/
+var ToolsList = React.createClass({
+  renderTool : function(key){
+    return <Tool key={key} index={key} details={this.props.tools[key]}/>
+  },
+  render : function() {
+    var tools = this.props.tools;
+    return (
+      <div className="row">
+        {Object.keys(tools).map(this.renderTool)}
+      </div>
+    )
+  }
+})
+
+/*
+  WrenchersHeader
+  Usage: <WrenchersHeader/>
+  Renders that beautiful Wrenchers branded header that you've come to know and love.
+*/
 var WrenchersHeader = React.createClass({
   render : function() {
     return (
@@ -99,62 +306,51 @@ var WrenchersHeader = React.createClass({
   }
 })
 
-var OtherComponent = React.createClass({
+/*
+  Footer
+  Usage: <Footer/>
+  Renders a Wrenchers footer with all sorts of juicy information.
+*/
+var Footer = React.createClass({
   render : function() {
     return (
-      <div></div>
-    )
-  }
-})
-
-var FeaturedComponent = React.createClass({
-  render : function() {
-    return (
-      <div></div>
-    )
-  }
-})
-
-var Components = React.createClass({
-  renderComponent : function(key){
-    return <OtherComponent key={key} index={key} details={this.props.components[key]}/>
-  },
-  render : function() {
-    console.log(this.props.components)
-    var featured = this.props.components.featuredComponent;
-    var other = this.props.components.otherComponents;
-    return (
-      <div>
-        {featured && <FeaturedComponent component={featured}/>}
-        {other && Object.keys(other).map(this.renderComponent)}
+      <footer id="footer">
+      <div className="trans-bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 text-center">
+               <div className="copyright">
+                <p>&copy; <span>The Wrenchers Project</span>2016, Some rights reserved</p>
+              </div>
+                <p>steven@wrenchers.co</p>
+            </div>
+            </div>
+          </div>
+        <div id="go-to-top">
+            <a href="#banner"></a>
+        </div>
       </div>
+      </footer>
     )
   }
 })
 
 /*
-  Header
-  <Header/>
+  NotFound
+  Usage: component={NotFound}
+  This should be some kinda 404 page for if someone finds themselves astray.
 */
-var Header = React.createClass({
-  render : function() {
-    return (
-      <header className="top">
-      </header>
-    )
-  }
-})
-
-/*
-  Not Found
-*/
-
 var NotFound = React.createClass({
   render : function() {
     return <h1>Not Found!</h1>
   }
 });
 
+/*
+  Home
+  Usage: component={Home}
+  This should be some kinda landing page for if someone goes to the site home page.
+*/
 var Home = React.createClass({
   render : function() {
     return (
@@ -167,7 +363,6 @@ var Home = React.createClass({
 /*
   Routes
 */
-
 var routes = (
   <Router history={createBrowserHistory()}>
     <Route path="/" component={Home}/>
